af347558f4ef296c46769d463a94d915252379b1
nvixonimo <36642137+nvixonimo@users.noreply.github.com>
2021-04-21 23:01:38 +0300

    Small recontructions and README updates

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/bash
 
 
-# Disabling yggdrasil in case it's already installed
-prepare () {
+# Disabling yggdrasil in case it's already launched
+check() {
     systemctl stop yggdrasil.service
     systemctl disable yggdrasil.service
 }
 
 # Installation
 install() {
-    # Add repository to your machine
+    # Add the repository
     gpg fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
     gpg export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | apt-key add -
     printf 'deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | tee /etc/apt/sources.list.d/yggdrasil.list
     
-    # Refresh pakage lists
+    # Refresh package lists and install
     apt update
-    apt install yggdrasil
+    apt install yggdrasil -y
 }
 
-# Enable the service
+# Enable and start the service
 service_on() {
     systemctl enable yggdrasil
     systemctl start yggdrasil
@@ -42,7 +42,11 @@ configs() {
 
 # Pull docker image from dockerhub
 docker_node_pull() {   
+    printf "\n[ Pulling docker image with emdedded yggdrasil ]\n"
+
     docker pull luzifer/yggdrasil
+
+    printf "\n[ Docker image pulled ]\n"
 }
 
 # Launch the container
@@ -50,41 +54,43 @@ docker_node_launch() {
     docker run --rm -ti --net=host --cap-add=NET_ADMIN --device=/dev/net/tun -v $(pwd):/config luzifer/yggdrasil
 }
 
+# Separate different stages of the script
+separate() {
+    printf "\n#####################################################################################\n"
+    printf "#####################################################################################\n"
+    printf "#####################################################################################\n"
+}
 
 # Launching the complete process
 printf "\n#####################################################################################\n"
 
 printf "\n[ Disabling potentially pre-launched yggdrasil ]\n\n"
-prepare
+check
 printf "\n[ Done ]\n"
 
-printf "\n#####################################################################################\n"
-printf "#####################################################################################\n"
-printf "#####################################################################################\n"
+separate
 
 printf "\n[ Install start ]\n\n"
 install
 printf "\n[ Install complete ]\n"
 
-printf "\n#####################################################################################\n"
-printf "#####################################################################################\n"
-printf "#####################################################################################\n"
+separate
 
-printf "\n[ Pulling docker image with emdedded yggdrasil ]\n\n"
-docker_node_pull
-printf "\n[ Docker image pulled ]\n"
+printf "\n[ Pull docker image with pre-installed yggdrasil? ] [ choose number ]\n"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) docker_node_pull; break;;
+        No ) printf "\n[ Docker image not pulled ]\n"; break;;
+    esac
+done
 
-printf "\n#####################################################################################\n"
-printf "#####################################################################################\n"
-printf "#####################################################################################\n"
+separate
 
 printf "\n[ Launching the service ]\n\n"
 service_on
 printf "\n[ Service is up ]\n"
 
-printf "\n#####################################################################################\n"
-printf "#####################################################################################\n"
-printf "#####################################################################################\n"
+separate
 
 printf "\n[ Generate new configs? ] [ choose number ]\n"
 select yn in "Yes" "No"; do
@@ -94,10 +100,9 @@ select yn in "Yes" "No"; do
     esac
 done
 
-printf "\n#####################################################################################\n"
-printf "#####################################################################################\n"
-printf "#####################################################################################\n"
+separate
 
+if [[ "$(docker images -q luzifer/yggdrasil)" ]]; then
 printf "\n[ Launch docker container? ] [ choose number ]\n"
 select yn in "Yes" "No"; do
     case $yn in
@@ -106,9 +111,11 @@ select yn in "Yes" "No"; do
     esac
 done
 
-printf "\n#####################################################################################\n"
-printf "#####################################################################################\n"
-printf "#####################################################################################\n"
+else
+printf "\n[ Docker image is not present, so no container is launched ]\n\n"
+fi
+
+separate
 
 
 printf "\n[ All done ]\n\n"
